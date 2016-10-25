432682d798d508e014cc726fa197baf31b89a95c
yanweiqing <ywq1991@gmail.com>
2016-10-25 10:08:46 +0800

    mocker sdk init

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var path = require('path');
+var rootPath = path.dirname(__dirname);
+var rooter = function(dir) {
+  return path.join(rootPath, dir);
+}
+
+module.exports = {
+  entry: [rooter('mocker-sdk/src/index.js')],
+  output: {
+    path: rooter('mocker-sdk/dest'),
+    filename: '[name].js',
+    chunkFilename: '[name].js'
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015', 'stage-0'],
+        plugins: [
+          'add-module-exports',
+          'transform-decorators-legacy',
+          ['transform-runtime', {
+            'polyfill': false,
+            'regenerator': true
+          }]
+        ]
+      }
+    }]
+  }
+}
