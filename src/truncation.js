20629655346d3f28cc8c6f90caa4bbbb6656e6d0
lanmaomm <lanmaomm@163.com>
2016-10-27 15:21:10 +0800

    demo on vue

--- src/truncation.js
+++ src/truncation.js
@@ -18,6 +18,36 @@ const truncation = function() {
     });
   }
 }
+const changeVueSettings = function(request) {
+    var isMockEnable = checkMockAvailability();
+    console.log(123);
+    if (!isMockEnable) {
+      return;
+    }
+    debugger;
+    let originUrl = request.url;
+    if (/\/\/localhost/.test(originUrl) || /\/\/127\.0\.0\.1/.test(originUrl)) {
+        return;
+    }
+
+    let mockInfo = getMockOption();
+    let originData = request.data;
+    let mockUrl = 'http://localhost:8787/mock';
+    let location = document.createElement('a');
+    location.href = originUrl;
+    let query = [
+      `project=${mockInfo.project}`,
+      `version=${mockInfo.version}`,
+      `api=${location.pathname.slice(1)}`,
+      `qs=${encodeURIComponent(location.search)}`,
+      `server=${location.origin}`
+    ]
+
+    request.url = mockUrl + '?' + query.join('&');
+    request.xhrFields = request.xhrFields || {};
+    request.xhrFields.withCredentials = true;
+    var async = 'async' in request ? request.async : true;
+}
 
 const changeSettings = function(event, xhr, settings) {
     let isMockEnable = checkMockAvailability();
